6cb54750ee85866120939bd8326d372e25018496
Pasha <pashakasm@mail.ru>
2022-09-11 10:05:33 +0300

    решение

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿string[] array = new string[] {"hello", "846", "55", "hi"};
+string[] array2 = new string[array.Length];
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i].Length <= 3)
+    {
+        array2[i] = array[i];
+    }        
+}
+for (int i = 0; i < array2.Length; i++)
+{
+    Console.Write(array2[i]+ " ");
+}
